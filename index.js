bc6f75180030ee9b8f9eb283cbeb92d275842087
iMocco <taoxin167@gmail.com>
2017-04-28 11:01:00 +0800

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: iMocco
 * @Date:   2017-03-16 
 * @Last Modified by:   iMocco
-* @Last Modified time: 2017-03-20 17:37:37
+* @Last Modified time: 2017-03-21 13:47:30
 */
 'use strict';
 var express = require('express')
@@ -10,6 +10,7 @@ var powerexpress = require('power-express')(express)
 var authority = require('./filter/authority')
 var app = powerexpress()
 var Server = require('./server');
+app.use(express.static(__dirname + '/public/'))
 app.use(authority.setCrossDomain)
 require('./controllers/routes')(app);
 var appServer = new Server(app);
