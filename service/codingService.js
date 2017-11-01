8f1a0212dbdb929d062332f377bfcf7ad9fab68a
iMocco <taoxin167@gmail.com>
2017-11-01 16:34:01 +0800

    添加删除数据和物理删除数据的接口

--- service/codingService.js
+++ service/codingService.js
@@ -1,9 +1,9 @@
 /*
-* @Author: iMocco
-* @Date:   2017-06-23 13:50:34
-* @Last Modified by:   iMocco
-* @Last Modified time: 2017-07-03 12:43:24
-*/
+ * @Author: iMocco
+ * @Date:   2017-06-23 13:50:34
+ * @Last Modified by:   iMocco
+ * @Last Modified time: 2017-07-03 12:43:24
+ */
 
 'use strict';
 var http = require('http');
@@ -18,132 +18,132 @@ var cheerio = require('cheerio');
 var CronJob = require('cron').CronJob
 class CodingService {
 
-	// 定时登录发布冒泡
-	runAddTweet () {
-		// let self = this
-    	// 每天早上9:00更新一次
-    	// var job = new CronJob('0 9 * * *', function () {
-    		// self.loginCoding()
-    	// }, null, true, 'Asia/Chongqing')
-    }
-    // 定时点赞
-    runLikePop(){
-    	let self = this
-    	// 每天早上9:00更新一次
-    	// var job = new CronJob('56 23 * * *', function () {
-    		self.likePop()
-    	// }, null, true, 'Asia/Chongqing')
+    // 定时登录发布冒泡
+    runAddTweet() {
+            // let self = this
+            // 每天早上9:00更新一次
+            // var job = new CronJob('0 9 * * *', function () {
+            // self.loginCoding()
+            // }, null, true, 'Asia/Chongqing')
+        }
+        // 定时点赞
+    runLikePop() {
+        let self = this
+            // 每天早上9:00更新一次
+            // var job = new CronJob('56 23 * * *', function () {
+        self.likePop()
+            // }, null, true, 'Asia/Chongqing')
     }
 
     // 登录
-    loginCoding(){
-    	var self = this
-    	var data = querystring.stringify({
-    		account:"18512115504",
-    		password:"2fd0da67cf3343c3f360c280be1d0a241d7c072e"
-    	});
-    	var options = {
-    		host: 'coding.net',
-    		path:'/api/v2/account/login',
-    		method: 'POST',
-    		headers: {
-    			'Content-Type': 'application/x-www-form-urlencoded',
-    			'Cookie':'sid='+ sid
-    		}
-    	};
-    	var reqq = https.request(options, function(ress) {
-    		ress.setEncoding('utf8');
-    		ress.on('data', function (chunk) {
-    			var result = JSON.parse(chunk)
-    			if(result.code === 0){
-    				self.addTweet()
-    			}
-    		});
-    		ress.on('end',function(chunk){
-    			console.log("body: " + chunk);
-    		})
-    	});
-    	reqq.write(data);
-    	reqq.end();
+    loginCoding() {
+        var self = this
+        var data = querystring.stringify({
+            account: "18512115504",
+            password: "2fd0da67cf3343c3f360c280be1d0a241d7c072e"
+        });
+        var options = {
+            host: 'coding.net',
+            path: '/api/v2/account/login',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie': 'sid=' + sid
+            }
+        };
+        var reqq = https.request(options, function(ress) {
+            ress.setEncoding('utf8');
+            ress.on('data', function(chunk) {
+                var result = JSON.parse(chunk)
+                if (result.code === 0) {
+                    self.addTweet()
+                }
+            });
+            ress.on('end', function(chunk) {
+                console.log("body: " + chunk);
+            })
+        });
+        reqq.write(data);
+        reqq.end();
     }
 
     // 发表冒泡
-    addTweet(){
-    	request('http://wufazhuce.com', function(error, response, body) {
-    		if (!error && response.statusCode == 200) {
-    			var $ = cheerio.load(body);
-    			var img = $('img.fp-one-imagen')['0'].attribs.src
-    			var text = $('.fp-one-cita a')[0].children[0].data
+    addTweet() {
+        request('http://wufazhuce.com', function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body);
+                var img = $('img.fp-one-imagen')['0'].attribs.src
+                var text = $('.fp-one-cita a')[0].children[0].data
 
-    			var data = querystring.stringify({
-    				content: '#早安# ' + text,
-    				location: 'Shang Hai',
-    				device: "iPhone 6S Plus"
-    			});
-    			var options = {
-    				host: 'coding.net',
-    				path:'/api/social/tweet',
-    				method: 'POST',
-    				headers: {
-    					'Content-Type': 'application/x-www-form-urlencoded',
-    					'Cookie':'sid=' + sid
-    				}
-    			};
-    			var reqq = https.request(options, function(ress) {
-    				ress.setEncoding('utf8');
-    				ress.on('data', function (chunk) {
-    					console.log(JSON.parse(chunk));
-    				});
-    				ress.on('end',function(chunk){
-    					console.log("body: " + chunk);
-    				})
-    			});
-    			reqq.write(data);
-    			reqq.end();
-    		}
-    	})
+                var data = querystring.stringify({
+                    content: '#早安# ' + text,
+                    location: 'Shang Hai',
+                    device: "iPhone 6S Plus"
+                });
+                var options = {
+                    host: 'coding.net',
+                    path: '/api/social/tweet',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Cookie': 'sid=' + sid
+                    }
+                };
+                var reqq = https.request(options, function(ress) {
+                    ress.setEncoding('utf8');
+                    ress.on('data', function(chunk) {
+                        console.log(JSON.parse(chunk));
+                    });
+                    ress.on('end', function(chunk) {
+                        console.log("body: " + chunk);
+                    })
+                });
+                reqq.write(data);
+                reqq.end();
+            }
+        })
     }
 
     // 点赞
-    likePop(){
-    	var time = new Date().getTime()
-    	https.get('https://coding.net/api/social/tweet/public_tweets?sort=time&last_time='+ time +'&default_like_count=10&size=100&filter=true', function(ress){
-    		var str = '';
-    		ress.on('data', function (chunks) {
-    			str += chunks;
-    		});
-    		ress.on('end', function () {
-    			var result = JSON.parse(str)
-    			if(result.code === 0){
-    				for (var i = 0; i < result.data.length; i++) {
-    					let id = result.data[i].id
-						var data = querystring.stringify({});
-						// 循环点赞
-						var options = {
-							host: 'coding.net',
-							path:'/api/social/tweet/'+ id +'/like',
-							method: 'POST',
-							headers: {
-								'Content-Type': 'application/x-www-form-urlencoded',
-								'Cookie':'sid='+sid
-							}
-						};
-						var req = https.request(options, function(resss) {
-							resss.setEncoding('utf8');
-							console.log(i +'----- '+resss.statusCode);
-							resss.on('data', function (chunk) {
-								console.log(i+": " + chunk);
-							});
-							resss.on('end',function(chunk){
-								console.log(i+": " + chunk);
-							})
-						});
-						req.write(data);
-						req.end();
-					}
-				}
-			});
-    	});
+    likePop() {
+        var time = new Date().getTime()
+        https.get('https://coding.net/api/social/tweet/public_tweets?sort=time&last_time=' + time + '&default_like_count=10&size=100&filter=true', function(ress) {
+            var str = '';
+            ress.on('data', function(chunks) {
+                str += chunks;
+            });
+            ress.on('end', function() {
+                var result = JSON.parse(str)
+                if (result.code === 0) {
+                    for (var i = 0; i < result.data.length; i++) {
+                        let id = result.data[i].id
+                        var data = querystring.stringify({});
+                        // 循环点赞
+                        var options = {
+                            host: 'coding.net',
+                            path: '/api/social/tweet/' + id + '/like',
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                                'Cookie': 'sid=' + sid
+                            }
+                        };
+                        var req = https.request(options, function(resss) {
+                            resss.setEncoding('utf8');
+                            console.log(i + '----- ' + resss.statusCode);
+                            resss.on('data', function(chunk) {
+                                console.log(i + ": " + chunk);
+                            });
+                            resss.on('end', function(chunk) {
+                                console.log(i + ": " + chunk);
+                            })
+                        });
+                        req.write(data);
+                        req.end();
+                    }
+                }
+            });
+        });
     }
 }
 module.exports = new CodingService()
--- storage/leancloudDao.js
+++ storage/leancloudDao.js
@@ -1,85 +1,97 @@
 /*
-* @Author: iMocco
-* @Date:   2017-03-16 16:59:33
-* @Last Modified by:   iMocco
-* @Last Modified time: 2017-10-27 04:13:39
-*/
+ * @Author: iMocco
+ * @Date:   2017-03-16 16:59:33
+ * @Last Modified by:   iMocco
+ * @Last Modified time: 2017-10-27 04:13:39
+ */
 'use strict'
 
 var AV = require('leanengine')
 
 class leancloudDao {
 
-	// 获取总数
-	getCount(table){
-		return new Promise(function (resolve, reject) {
-			const query = new AV.Query(table)
-			query.count().then(function (count) {
-				resolve(count)
-			}, function (error) {
-				reject(err)
-			})
-		}.bind(this))
-	}
+    // 获取总数
+    getCount(table) {
+        return new Promise(function(resolve, reject) {
+            const query = new AV.Query(table)
+            query.count().then(function(count) {
+                resolve(count)
+            }, function(error) {
+                reject(err)
+            })
+        }.bind(this))
+    }
 
-	// 获取分页数据
-	getDataFindPage(table, page, params, timeParam, startTime, endTime, sortBy, sort){
-		return new Promise(function (resolve, reject) {
-			const query = new AV.Query(table)
-			
-			// 返回条数
-			query.limit(JSON.parse(page).limit || 20);
-			// 查询起始位置
-			query.skip(JSON.parse(page).start || 0);
-			// // 开始时间
-			query.greaterThanOrEqualTo(timeParam || 'createdAt', new Date(startTime || '1900-01-01'));
-			// // 结束时间
-			query.lessThanOrEqualTo(timeParam || 'createdAt', new Date(endTime || '2300-01-01'));
-			if(params){
-				var newParams = JSON.parse(params)
-				for(var key in newParams){
-					query.equalTo(key, newParams[key]);
-				}
-			}
-			if(sort){
-				switch(sort) {
-					case 1:
-					query.descending( timeParam || 'createdAt');
-					break;
-					case -1:
-					query.ascending( timeParam || 'createdAt');
-					break;
-				}
-			}else{
-				query.descending( timeParam || 'createdAt');
-			}
-			query.find().then(function (result) {
-				if(result){
-					query.count().then(function (count) {
-						resolve({
-							total:count,
-							records: result
-						})
-					}, function (error) {
-						reject(err)
-					})
-				}
+    // 获取分页数据
+    getDataFindPage(table, page, params, timeParam, startTime, endTime, sortBy, sort) {
+        return new Promise(function(resolve, reject) {
+            const query = new AV.Query(table)
 
-			}, function (error) {
-				reject(err)
-			})
-		}.bind(this))
-	}
+            // 返回条数
+            query.limit(JSON.parse(page).limit || 20)
+                // 查询起始位置
+            query.skip(JSON.parse(page).start || 0)
+                // // 开始时间
+            query.greaterThanOrEqualTo(timeParam || 'createdAt', new Date(startTime || '1900-01-01'))
+                // // 结束时间
+            query.lessThanOrEqualTo(timeParam || 'createdAt', new Date(endTime || '2300-01-01'))
+            if (params) {
+                var newParams = JSON.parse(params)
+                for (var key in newParams) {
+                    query.equalTo(key, newParams[key])
+                }
+            }
+            query.equalTo('isDeleted', false)
+            if (sort) {
+                switch (sort) {
+                    case 1:
+                        query.descending(timeParam || 'createdAt')
+                        break
+                    case -1:
+                        query.ascending(timeParam || 'createdAt')
+                        break
+                }
+            } else {
+                query.descending(timeParam || 'createdAt')
+            }
+            query.find().then(function(result) {
+                if (result) {
 
-	// 删除数据
-	deleteRecord(table, objectId){
-		return new Promise(function (resolve, reject) {
-			var  deleteData= AV.Object.createWithoutData(table, objectId);
-			deleteData.destroy().then(function (success) {
-				resolve(success);
-			}, function (error) {
-			});
-		}.bind(this))
-	}
+                    // 获取总数
+                    query.count().then(function(count) {
+                        resolve({
+                            total: count,
+                            records: result
+                        })
+                    }, function(error) {
+                        reject(err)
+                    })
+                }
+            }, function(error) {
+                reject(err)
+            })
+        }.bind(this))
+    }
+
+    // 删除数据
+    removeRecord(table, objectId) {
+        return new Promise(function(resolve, reject) {
+            var removeData = AV.Object.createWithoutData(table, objectId)
+            removeData.destroy().then(function(success) {
+                resolve(success)
+            }, function(error) {})
+        }.bind(this))
+    }
+
+    // 物理删除数据
+    deleteRecord(table, objectId) {
+        return new Promise(function(resolve, reject) {
+            var deleteData = AV.Object.createWithoutData(table, objectId)
+            deleteData.set('isDeleted', true)
+            deleteData.save()
+            resolve(true)
+        }.bind(this))
+    }
+
 }
 module.exports = new leancloudDao()
